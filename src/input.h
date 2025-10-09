4e5bdac490d63cce0f18dc71ca206b1a1ba44058
Frank <101807917+frankischilling@users.noreply.github.com>
2025-10-09 13:03:27 -0400

    Add clipboard_get_data function declaration
    Added function declaration for clipboard_get_data.

--- src/input.h
+++ src/input.h
@@ -7,5 +7,6 @@ void handle_keypress(Display *display, Window window, XEvent *event);
 void handle_paste_event(Display *display, Window window, XEvent *event);
 void copy_to_clipboard(Display *display, Window window);  // Added declaration
 void paste_from_clipboard(Display *display, Window window);  // Added declaration
+const unsigned char *clipboard_get_data(size_t *len_out);
 
 #endif // INPUT_H
